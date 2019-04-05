859e0973d3982f56ff4290fdb5e28ae210be05a6
Jarred <bettersoundingonhugs@gmail.com>
2019-04-05 16:38:03 +1300

    Second Commit

---
+++ Assignment1/Assignment1/Assignment1/Page3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Assignment1
+{
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            editor.Text = "";
+            Button button = (Button)sender;
+            await DisplayAlert("Thank you", "Your Feedback has been sent", "OK");
+        }
+    }
+}
